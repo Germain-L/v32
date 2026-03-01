1d4e4382d32475838dd8fa6d1a5e8ceec48477b7
Germain Leignel <g.leignel@groupeonepoint.com>
2026-03-01 18:17:16 +0100

    feat: trigger image migration on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'app.dart';
+import 'data/services/image_migration_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   GoogleFonts.config.allowRuntimeFetching = false;
+
+  // Run migration for existing images (one-time)
+  await ImageMigrationService.migrateExistingImages();
+
   runApp(const DietApp());
 }
