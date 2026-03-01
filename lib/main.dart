5b26d4f7055d2693ea84466dc93dbc5f45855b20
Germain Leignel <g.leignel@groupeonepoint.com>
2026-03-01 18:18:53 +0100

    chore: remove migration trigger from startup (clean build)

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'app.dart';
-import 'data/services/image_migration_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   GoogleFonts.config.allowRuntimeFetching = false;
-
-  // Run migration for existing images (one-time)
-  await ImageMigrationService.migrateExistingImages();
 
   runApp(const DietApp());
 }
